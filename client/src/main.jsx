f4309349741c559ae5cc1781ab6c1730947f0c0a
hygiehelm-hub <hygiehelm@gmail.com>
2026-01-09 11:38:42 +0530

    Fix: Restore ReactDOM import causing blank screen

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,3 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { AuthProvider } from "./context/AuthContext";
+import "./styles.css";
+
 import { GoogleOAuthProvider } from '@react-oauth/google';
 
 // ... (other imports)
